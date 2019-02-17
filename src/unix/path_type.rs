7fe3184d1fd1e62772ca52e20553fc6b975c3133
Ariel De Ocampo <arielmakestuff@gmail.com>
2019-02-17 09:27:00 -0500

    Add new type: Separator
    In order to parse unix-style paths, there is a need to be able to
    detect the separator character. Currently, there is no way beyond
    comparing against a character directly.
    
    This change adds a new type called `Separator` that matches against the
    unix style path separator.

--- src/unix/path_type.rs
+++ src/unix/path_type.rs
@@ -12,6 +12,58 @@
 // Third-party imports
 
 // Local imports
+use crate::mk_reverse_equal;
+
+// ===========================================================================
+// Path Prefix Types: Separator
+// ===========================================================================
+
+#[derive(Debug)]
+pub struct Separator;
+
+impl PartialEq<&[u8]> for Separator {
+    fn eq(&self, other: &&[u8]) -> bool {
+        return other == b"/";
+    }
+}
+
+mk_reverse_equal!(Separator, &[u8]);
+
+// ===========================================================================
+// Tests
+// ===========================================================================
+
+#[cfg(test)]
+mod test {
+    mod separator {
+        use crate::unix::path_type::Separator;
+
+        use proptest::{
+            prop_assert, prop_assert_ne, prop_assume, proptest, proptest_helper,
+        };
+
+        #[test]
+        fn self_equal() {
+            assert_eq!(Separator, Separator);
+        }
+
+        #[test]
+        fn equal_to_sep() {
+            let s = b"/";
+            assert_eq!(Separator, &s[..]);
+        }
+
+        proptest! {
+            #[test]
+            fn invalid_value(s in r#".*"#) {
+                prop_assume!(s.len() != 1 || s != "/");
+                let arr: Vec<u8> = s.bytes().map(|c| c as u8).collect();
+                prop_assert_ne!(Separator, &arr[..]);
+            }
+        }
+
+    }
+}
 
 // ===========================================================================
 //

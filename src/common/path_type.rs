00392eb96d60ef89223701d1a83e2974b2858093
Ariel De Ocampo <arielmakestuff@gmail.com>
2019-02-17 09:49:25 -0500

    Move unit tests from windows::path_type to common::path_type
    The unit tests for the CurrentDir and ParentDir types are still in the
    windows::path_type module. This is confusing since these types are now
    defined in the common::path_type module.
    
    This change moves these unit tests to the common::path_type module where
    the CurrentDir and ParentDir types are defined.

--- src/common/path_type.rs
+++ src/common/path_type.rs
@@ -79,6 +79,77 @@ impl PartialEq<&[u8]> for ParentDir {
 
 mk_reverse_equal!(ParentDir, &[u8]);
 
+#[cfg(test)]
+mod test {
+    mod currentdir {
+        use crate::common::path_type::CurrentDir;
+
+        use proptest::{
+            prop_assert, prop_assert_ne, prop_assume, proptest, proptest_helper,
+        };
+
+        #[test]
+        fn self_equal() {
+            assert_eq!(CurrentDir, CurrentDir);
+        }
+
+        #[test]
+        fn valid_value() {
+            let dir = ".";
+            assert_eq!(dir.as_bytes(), CurrentDir);
+        }
+
+        #[test]
+        fn as_str() {
+            assert_eq!(CurrentDir::as_str(), ".");
+        }
+
+        proptest! {
+            #[test]
+            fn invalid_value(dir in r#"[^/\\]"#) {
+                prop_assume!(dir != ".");
+
+                let dir_bytes = dir.as_bytes();
+                prop_assert_ne!(dir_bytes, CurrentDir);
+            }
+        }
+    }
+
+    mod parentdir {
+        use crate::common::path_type::ParentDir;
+
+        use proptest::{
+            prop_assert, prop_assert_ne, prop_assume, proptest, proptest_helper,
+        };
+
+        #[test]
+        fn self_equal() {
+            assert_eq!(ParentDir, ParentDir);
+        }
+
+        #[test]
+        fn valid_value() {
+            let dir = "..";
+            assert_eq!(dir.as_bytes(), ParentDir);
+        }
+
+        #[test]
+        fn as_str() {
+            assert_eq!(ParentDir::as_str(), "..");
+        }
+
+        proptest! {
+            #[test]
+            fn invalid_value(dir in r#"[^/\\]"#) {
+                prop_assume!(dir != "..");
+
+                let dir_bytes = dir.as_bytes();
+                prop_assert_ne!(dir_bytes, ParentDir);
+            }
+        }
+    }
+}
+
 // ===========================================================================
 //
 // ===========================================================================
